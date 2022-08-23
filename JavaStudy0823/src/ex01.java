d57d36a9a5251c934bd6f6fb2594f5e23dcb0e6c
smhrd <smhrd@121.147.52.4>
2022-08-23 15:52:51 +0900

    커밋

--- JavaStudy0823/src/ex01.java
+++ JavaStudy0823/src/ex01.java
@@ -7,13 +7,17 @@ public class ex01 {
 		// --> 사용자가 입력한 내용을 출력시키는 명령어
 		System.out.println("JAVA");
 		System.out.println("DB");
-		//숫자 10을 출력 시키고 싶습니다!
-		//문자열 + 숫자 => 붙여져서 출력 되어진다!
+		
+		// 숫자 10을 출력 시키고 싶습니다!
+		// 문자열 + 숫자 => 붙여져서 출력 되어진다!
 		// 문자열 + 문자열 => 붙여져서 출력 되어진다!
 		System.out.println("10" + "3");
 		
-		System.out.println(10+3+"2");
-		System.out.println("2"+3+10);
+		System.out.println(10+3+"2"); // 132
+		System.out.println("2"+3+10); // 2310
+		
+		//syso + Ctrl + Space -> 자동완성
+		System.out.println();
 	}
 
 }
