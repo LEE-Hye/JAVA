fcc2ed685adc693c3089e36fa903b92576113ba1
smhrd <smhrd@121.147.52.4>
2022-08-23 15:35:55 +0900

    첫번재 자바 커밋

---
+++ JavaStudy0823/src/ex01.java
@@ -0,0 +1,13 @@
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		// 설명달기! -> 주석달기!
+		// System.out.println("내용")
+		// --> 사용자가 입력한 내용을 출력시키는 명령어
+		System.out.println("JAVA");
+		System.out.println("DB");
+
+	}
+
+}
