b847544641b24e8a14daec9093f935f563e7b413
smhrd <smhrd@121.147.52.4>
2022-08-25 18:00:04 +0900

    다중 if문 switch case로 바꾸기

--- JavaStudy0825/src/ex03.java
+++ JavaStudy0825/src/ex03.java
@@ -21,7 +21,19 @@ public class ex03 {
 		else {
 			System.out.println("휴학 추천합니다 ♪(^∇^*)");
 		}
-	
+		
+		switch(grade) {
+		case 100,99,98,97,96,95,94,93,92,91,90:
+			System.out.println("A학점 입니다.");
+			break;
+		case 89,88,87,86,85,84,83,82,81,80:
+			System.out.println("B학점 입니다.");
+			break;
+		case 79,78,77,76,75,74,73,72,71,70:
+			System.out.println("C학점 입니다.");
+			break;
+		default:
+			System.out.println("휴학 추천할게용");
+		}
 	}
-
 }
