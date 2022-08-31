9f369c611a067331b2a5f29ea065e68470ff6491
smhrd <smhrd@121.147.52.4>
2022-08-31 16:16:24 +0900

    2022-08-31 기본 반복문 완료

--- JavaStudy8030/src/for문_예제/ex04.java
+++ JavaStudy8030/src/for문_예제/ex04.java
@@ -7,7 +7,7 @@ public class ex04 {
 	public static void main(String[] args) {
 		
 		// 입력 받은 정수의 약수를 구하세요.
-		
+		System.out.println("문제1.");
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("정수를 입력하세요 : ");
@@ -19,6 +19,20 @@ public class ex04 {
 			}
 		}
 
+		
+		// 2~30까지의 약수를 구하세요
+		System.out.println("문제2.");
+		for (int i = 2; i <= 30; i++) {
+			System.out.print(i + "의 약수 : ");
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					System.out.print(j + " ");
+				}
+			}
+			System.out.println();
+		}
+
+		// 코드 정리 : Ctrl + A -> Ctrl + Shift + F
 	}
 
 }
