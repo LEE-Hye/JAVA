e05a72b004f7dbe07f560c5017163f8eb0aeb8df
smhrd <smhrd@121.147.52.4>
2022-09-05 12:16:50 +0900

    2022-09-05 오전 마지막 커밋

--- JavaStudy0905/src/ContactMain.java
+++ JavaStudy0905/src/ContactMain.java
@@ -3,14 +3,20 @@ public class ContactMain {
 
 	public static void main(String[] args) {
 
-		Contact ct = new Contact ("Lee", 20, "010-1234-5678");
+		Contact ct1 = new Contact ("신명호", 23, "010-1745-2788");
+		Contact ct2 = new Contact ("김명현", 22, "010-6450-5798");
+		Contact ct3 = new Contact ("전수현", 21, "010-7086-4056");
+		Contact ct4 = new Contact ("나소영", 20, "010-5464-7860");
 
-		String name = ct.getName();
-		int age = ct.getAge();
-		String tel = ct.getTel();
+		Contact[] contact = {ct1, ct2, ct3, ct4}; 
 		
-		// %s를 대문자로 적으면 데이터도 대문자로 바뀐다
-		System.out.printf("%S(%d세) :  %s" , name, age, tel);
+		for(int i = 0; i < contact.length; i++) {
+			String name = contact[i].getName();
+			int age = contact[i].getAge();
+			String tel = contact[i].getTel();
+			System.out.printf("%s(%d세) :  %s%n" , name, age, tel);
+		}
+
 	}
 
 }
