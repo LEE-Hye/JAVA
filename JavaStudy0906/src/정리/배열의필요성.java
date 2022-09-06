b14d71ee952e57f7724bd65b22c440676dce46e5
smhrd <smhrd@121.147.52.4>
2022-09-06 14:30:08 +0900

    커밋

---
+++ JavaStudy0906/src/정리/배열의필요성.java
@@ -0,0 +1,18 @@
+package 정리;
+
+public class 배열의필요성 {
+
+	public static void main(String[] args) {
+
+		String id1 = "person1";
+		String pw1 = "person1";
+		
+		String id2 = "person2";
+		String pw2 = "person2";
+		
+		String id3 = "person3";
+		String pw3 = "person3";
+
+	}
+
+}
