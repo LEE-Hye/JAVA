c4b7ad2c9350183e611a680feecea3e17f1dcb5f
이혜빈 <leebin9295@naver.com>
2022-09-06 15:23:14 +0900

    잔디 심어지는지 확인

---
+++ JavaStudy0906/src/정리/Main.java
@@ -0,0 +1,17 @@
+package 정리;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// 1) 이름을 저장할 수 있는 배열 생성하고 팀원들 이름 저장하기
+		String[] team = {"신명호", "김명현", "이혜빈", "전수현", "나소영"};
+		
+		// 2) 지금 먹고 싶은 음식 3개 저장하기
+		String[] food = new String[3];
+		food[0] = "샤브샤브";
+		food[1] = "아구찜";
+		food[2] = "칼국수";
+	}
+
+}
