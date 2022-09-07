0441c545c7b03da40e711f0332ef1209538d9548
이혜빈 <leebin9295@naver.com>
2022-09-07 09:58:26 +0900

    커밋

---
+++ JavaStudy0907/src/Address.java
@@ -0,0 +1,18 @@
+
+public class Address {
+	
+	// ÁÖ¼Ò·Ï 
+	private String name;
+	private int mail;
+	private String tel;
+
+	public String getName() {
+		return name;
+	}
+	public int getMail() {
+		return mail;
+	}
+	public String getTel() {
+		return tel;
+	}
+}
