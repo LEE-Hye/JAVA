e7c6d78fb7f493ca5eb216e8043670d757a9e3f4
이혜빈 <leebin9295@naver.com>
2022-09-07 12:47:28 +0900

    2022-09-07 mp3 프로젝트

--- JavaStudy0907/src/Mp3.java
+++ JavaStudy0907/src/Mp3.java
@@ -4,9 +4,9 @@ public class Mp3 {
 	private String singer;
 	private String title;
 	private int playTime;
-	private String path; // ìŒì•…ê²½ë¡œ
+	private String path; // À½¾Ç°æ·Î
 	
-	// getter ë©”ì†Œë“œ
+	// getter ¸Ş¼Òµå
 	public String getSinger() {
 		return singer;
 	}
@@ -20,7 +20,7 @@ public class Mp3 {
 		return path;
 	}
 	
-	// ìƒì„±ì
+	// »ı¼ºÀÚ
 	public Mp3(String singer, String title, int playTime, String path) {
 		this.singer = singer;
 		this.title = title;
