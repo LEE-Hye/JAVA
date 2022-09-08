7bbf4f4bb8fe75b3a068b6e39c60148fba965225
이혜빈 <leebin9295@naver.com>
2022-09-08 17:43:40 +0900

    2022-09-08 덧셈게임

---
+++ JavaStudy0908/src/덧셈게임/Main.java
@@ -0,0 +1,44 @@
+package 덧셈게임;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		Random ran = new Random();
+
+		String[] fails = { "~\\(≧▽≦)/~", ".·´¯`(>▂<)´¯`·. ", "┗|｀O′|┛" };
+		
+		int i = 0;
+		int j = 20;
+		int k = 100;
+		
+		while (true) {
+
+			for (i = 0; i < fails.length; i++) {
+				int ran1 = ran.nextInt(j) + 1;
+				int ran2 = ran.nextInt(k) + 1;
+				System.out.printf("[%d번 문제] \n",i+1);
+				System.out.print( ran1 + "+" + ran2 + " = ");
+
+				int num = sc.nextInt();
+
+				if ((ran1 + ran2) == num) {
+					System.out.println(i + 1 + "번만에 성공~");
+					System.out.println("(✿ﾟ▽ﾟ)ノ");
+					break;
+				} else {
+					System.out.println(fails[i]);
+					System.out.println();
+				}
+			}
+			break;
+		}
+
+	}
+
+}
